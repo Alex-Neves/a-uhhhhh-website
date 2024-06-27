d0334ddbfc418076140eb27c88eb0cdd66ee3e1f
Doofus <92062410+Alex-Neves@users.noreply.github.com>
2024-06-27 14:33:38 -0500

    Switching from static html to react with vite+sass
    I've made my job harder but it will pay off.

---
+++ Franchising.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Franchising() {
+  return (
+    <div>Franchising</div>
+  )
+}
+
+export default Franchising
---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='Header'>
+      <a ><img alt='Big Dans Logo' src=''></img></a>
+    </div>
+  )
+}
+
+export default Header
